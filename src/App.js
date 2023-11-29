ff38d30586ce5c1ad831040ce000b7ced4d6d854
Tharunlakshmanan <106159594+Tharunlakshmanan@users.noreply.github.com>
2023-11-29 14:09:42 +0530

    added few logics for start and reset function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
-
 function App() {
+  // Using usestate hooks to setting the time and running or not
+  const [isRunning, setIsRunning] = useState(false);
+  const [time, setTime] = useState(0);
+  // using useeffect to effects of the components and set up a time
+  useEffect(() => {
+    let interval;
+    // defining the condition here and return
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTime((prev) => prev + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
 
-  // Storing value using use state
-  const [ time, setTime] = useState({ms:0,s:0,m:0})
+    return () => clearInterval(interval);
+  }, [isRunning]);
 
+  const handleStartStop = () => {
+    setIsRunning((prevIsRunning) => !prevIsRunning);
+  };
 
-  return <div className="App">
-    <h1>Stopwatch</h1>
-    <div className="stopwatch-card">
-      <p>00.00.0000</p>
-      <div className="buttons">
-        <button>Start</button>
-        <button>Stop</button>
-        <button>Pause</button>
+  const handleReset = () => {
+    setTime(0);
+    setIsRunning(false);
+  };
+
+  return (
+    <div className="App">
+      <h1>Stopwatch</h1>
+      <div className="stopwatch-card">
+        {/* set time in this section */}
+        <p>{time}</p>
+        <div className="buttons">
+          <button onClick={handleStartStop}>
+            {isRunning ? "Stop" : "Start"}
+          </button>
+          <button onClick={handleReset}>Reset</button>
+          <button>Pause</button>
+        </div>
       </div>
     </div>
-  </div>;
+  );
 }
 
 export default App;
